bfe3c38b28207cf358b8ca368081eb7b81965248
schougaardl <schougaardl@educ.college-montreal.qc.ca>
2025-09-26 14:53:04 -0400

    commit 4

--- main.py
+++ main.py
@@ -5,61 +5,70 @@ import random
 
 vie = 20
 
-
-
 while True:
     nb_victoire_consecutive = 0
     victoire = 0
     defaite = 0
     nb_combat = 0
-    force_monstre = random.randint(1, 5)
-    print(f"vous tomber face a un adeversaire de difficulte :{force_monstre}")
-    action = int(input("Que voulez-vous faire? \n 1-Combatre cet adversaire.\n 2-Contourner cet adversaire et aller ouvrir une autre porte.(coute un point de vie) \n 3-Afficher les règles du jeu. \n 4-quiter la partie \n Entrer votre choix:"))
+    while True:
 
-    if action == 1 :
-        nb_combat += 1
-        combat_status = False
-        score_de = random.randint(2, 12)
-        print(f"Adversaire : ")
-        print(f"Force de l'adversaire : {force_monstre}")
-        print(f"Niveau de vie de l'usager : {vie}")
-        print(f"Combat numéro {nb_combat} : {victoire} vs {defaite}.")
-        print(f"Lancer du dé : {score_de}.")
-        if score_de > force_monstre :
-            combat_status = True
+        force_monstre = random.randint(1, 12)
+        if nb_combat != 0 and nb_combat != 1 and victoire % 3 == 0:
+            force_monstre = 9000
+        print(f"vous tomber face a un adeversaire de difficulte :{force_monstre}")
+        action = int(input("Que voulez-vous faire? \n 1-Combatre cet adversaire.\n 2-Contourner cet adversaire et "
+                           "aller ouvrir une autre porte.(coute un point de vie) \n 3-Afficher les règles du jeu. "
+                           "\n 4-quiter la partie \n Entrer votre choix: \n "))
 
-        if combat_status == True:
-            print(f"Dernier combat : victoire ")
-            victoire += 1
-            nb_victoire_consecutive += 1
-            vie += force_monstre
-            print(f"Niveau de vie : {vie}.")
-            print(f"Nombre de victoire consecutive : {nb_victoire_consecutive}.")
+        if action == 1:
 
-        elif combat_status == False :
-            print(f"Dernier combat : defaite ")
-            defaite += 1
-            nb_victoire_consecutive = 0
-            vie -= force_monstre
-            print(f"Niveau de vie : {vie}.")
-            print(f"Nombre de victoire consecutive : {nb_victoire_consecutive}.")
+            nb_combat += 1
+            combat_status = False
+            score_de = random.randint(2, 12)
+            print(f"Lancer du dé : {score_de}.")
+            print(f"Adversaire : ")
+            print(f"Force de l'adversaire : {force_monstre}")
+            print(f"Niveau de vie de l'usager : {vie}")
+            print(f"Combat numéro {nb_combat} : {victoire} victoire(s) vs {defaite} défaite(s).")
 
-            if vie <= 0 :
-                print(f"La partie est terminé, vous avez vaincus {victoire} montre(s).")
-                break
+            if score_de > force_monstre:
+                combat_status = True
 
-    if action == 2 :
-        break
+            if combat_status:
+                print(f"Dernier combat : victoire ")
+                victoire += 1
+                nb_victoire_consecutive += 1
+                vie += force_monstre
+                print(f"Niveau de vie : {vie}.")
+                print(f"Nombre de victoire consecutive : {nb_victoire_consecutive}. \n ")
 
-    if action == 3 :
-        print("Pour réussir un combat, il faut que la valeur du dé lancé soit supérieure à la force de l’adversaire.  Dans ce cas, le niveau de vie de l’usager est augmenté de la force de l’adversaire.")
-        print("Une défaite a lieu lorsque la valeur du dé lancé par l’usager est inférieure ou égale à la force de l’adversaire.  Dans ce cas, le niveau de vie de l’usager est diminué de la force de l’adversaire.")
-        print("La partie se termine lorsque les points de vie de l’usager tombent sous 0.")
-        print("L’usager peut combattre ou éviter chaque adversaire, dans le cas de l’évitement, il y a une pénalité de 1 point de vie.")
+            else:
+                print(f"Dernier combat : defaite ")
+                defaite += 1
+                nb_victoire_consecutive = 0
+                vie -= force_monstre
+                print(f"Niveau de vie : {vie}.")
+                print(f"Nombre de victoire consecutive : {nb_victoire_consecutive}. \n")
 
-    if action == 4 :
+                if vie <= 0:
+                    print(f"La partie est terminé, vous avez vaincus {victoire} montre(s). \n ")
+                    break
+            print(f"Combat numéro {nb_combat} : {victoire} victoire(s) vs {defaite} défaite(s).\n")
+
+        if action == 2:
+            vie -= 1
+            print(f"Vous aver maintenant {vie} vie(s). \n ")
+
+
+    if action == 3:
+        print("Pour réussir un combat, il faut que la valeur du dé lancé soit supérieure à la force de l’adversaire. \n"
+              " Dans ce cas, le niveau de vie de l’usager est augmenté de la force de l’adversaire.\n"
+              "Une défaite a lieu lorsque la valeur du dé lancé par l’usager est inférieure ou égale à la force de "
+              "l’adversaire. \n Dans ce cas, le niveau de vie de l’usager est diminué de la force de l’adversaire. \n"
+              "La partie se termine lorsque les points de vie de l’usager tombent sous 0.\n "
+              "L’usager peut combattre ou éviter chaque adversaire, dans le cas de l’évitement, il y a une pénalité de"
+              " 1 point de vie. \n ")
+
+    if action == 4:
         print(" merci et aurevoire ... ")
         break
-
-
-
